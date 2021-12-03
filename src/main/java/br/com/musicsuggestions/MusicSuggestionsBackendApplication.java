a94626f344dc3697c0a7be94de868c5a6ad78839
Jonathan Souza <souzajonathan570@gmail.com>
2021-12-02 21:13:06 -0300

    first commit

---
+++ src/main/java/br/com/musicsuggestions/MusicSuggestionsBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.musicsuggestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicSuggestionsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicSuggestionsBackendApplication.class, args);
+	}
+
+}
