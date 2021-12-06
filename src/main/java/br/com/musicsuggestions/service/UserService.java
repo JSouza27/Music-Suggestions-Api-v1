bb4308225c4fd7502e3f8d32fd98fca28abfc1ba
Jonathan Souza <souzajonathan570@gmail.com>
2021-12-06 16:40:51 -0300

    feat: added validation users

--- src/main/java/br/com/musicsuggestions/service/UserService.java
+++ src/main/java/br/com/musicsuggestions/service/UserService.java
@@ -1,11 +1,14 @@
 package br.com.musicsuggestions.service;
 
 import br.com.musicsuggestions.dto.MessageResponseDTO;
+import br.com.musicsuggestions.dto.UserDTO;
 import br.com.musicsuggestions.model.User;
 import br.com.musicsuggestions.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,11 +20,23 @@ public class UserService {
     this.userRepository = userRepository;
   }
 
-  public MessageResponseDTO create(User newUser) {
-    User saveUser = userRepository.save(newUser);
+  public MessageResponseDTO create(UserDTO userDTO) {
+    User userToSave = User.builder()
+      .firstName(userDTO.getFirstName())
+      .LastName(userDTO.getLastName())
+      .email(userDTO.getEmail())
+      .passwrod(userDTO.getPasswrod())
+      .build();
+    User saveUser = userRepository.save(userToSave);
 
     return MessageResponseDTO.builder()
       .message("Usuário criado com sucesso " + saveUser.getId())
       .build();
+  }
+
+  public User findById(Long id) {
+    Optional<User> optionalUser = userRepository.findById(id);
+
+    return optionalUser.get();
   }
 }
